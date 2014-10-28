4780c5865fec28ebeb961f8174f5a620f2b6b379
Florian Hartig <florianhartig@users.noreply.github.com>
2014-10-28 17:44:22 +0100

    stats things

--- Labs/R/Practicals/StatisticalAnalysisInR/t.test.R
+++
@@ -1,8 +0,0 @@
-# Classical example from Student (1908) The probable error of the mean. Biometrika, 6, 20. The data which show the effect of two soporific drugs (increase in hours of sleep compared to control) on 10 patients. 
-
-hist(extra ~ group, data = sleep, xlab = "Treatment", ylab = "Extra hours sleep")
-
-## Traditional interface
-with(sleep, t.test(sleep$extra[sleep$group == 1], extra[group == 2]))
-## Formula interface
-t.test(extra ~ group, data = sleep)
